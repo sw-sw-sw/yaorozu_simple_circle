e73fe8103cdc69a4af79439b2b9c57c62a403e45
sw-sw-sw <35188370+sw-sw-sw@users.noreply.github.com>
2024-08-23 19:02:31 +0900

    微調整+readmeを更新

--- test_m1_gpu.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-with tf.device('/GPU:0'):
-    a = tf.constant([[1.0, 2.0], [3.0, 4.0]])
-    b = tf.constant([[1.0, 1.0], [0.0, 1.0]])
-    c = tf.matmul(a, b)
-
-print(c)
-print(c.device)  # '/job:localhost/replica:0/task:0/device:GPU:0' のように表示されるはず
